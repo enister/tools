43effdbfb04f54f1953b9dc50f5b0fe9617fa146
encomp <encomp>
2015-12-27 21:47:18 +0300

    Add druggable panel

---
+++ DraggablePanel/src/com/example/draggablepanel/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.draggablepanel;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button btn = (Button) findViewById(R.id.btn1);
+		final LinearLayout ll = (LinearLayout) findViewById(R.id.frame2);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (ll.getVisibility() == View.GONE)
+					ll.setVisibility(View.VISIBLE);
+				else
+					ll.setVisibility(View.GONE);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
